5e9a20151a1e1c95faaae1bacb5da66aec3246c8
AVTahion <avkontakt40@gmail.com>
2019-11-23 11:20:50 +0300

    Первые сущности и тестовые данные

---
+++ WebStore.Domain/Entities/Section.cs
@@ -0,0 +1,11 @@
+﻿using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfases;
+
+namespace WebStore.Domain.Entities
+{
+    public class Section : NamedEntity, IOrderedEntity
+    {
+        public int Order { set; get; }
+        public int? ParentId { set; get; }
+    }
+}
---
+++ WebStore/infrastucture/Services/InMemoryProductData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.infrastucture.interfaces;
+
+namespace WebStore.infrastucture.Services
+{
+    public class InMemoryProductData : IProductData
+    {
+    }
+}
