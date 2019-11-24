37636b4e8c3b8aa7e543a2ea92ef66a1240361fe
AVTahion <avkontakt40@gmail.com>
2019-11-24 15:18:05 +0300

    Визуальные компоненты

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.infrastucture.interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProductData _ProductData;
+
+        public BrandsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+
+        public IViewComponentResult Invoke() => View(GetBrands());
+
+        private IEnumerable<BrandViewModel> GetBrands() => _ProductData
+            .GetBrands()
+            .Select(brand => new BrandViewModel
+            {
+                Id = brand.Id,
+                Name = brand.Name,
+                Order = brand.Order
+            })
+            .OrderBy(brand => brand.Order)
+            .ToList();
+       
+    }
+}
