9a3fbf52bd0cccd3d9499239f7cfb04b5856d8d9
AVTahion <avkontakt40@gmail.com>
2019-11-25 17:07:24 +0300

    Новый проект для связи с ДБ, NuGet пакеты, доработка сущностей для размещения в ДБ.

--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfases;
 
 namespace WebStore.Domain.Entities
 {
+    [Table("Brands")]
     public class Brand : NamedEntity, IOrderedEntity
     {
         public int Order { set; get; }
+
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
--- WebStore.Domain/Entities/Section.cs
+++ WebStore.Domain/Entities/Section.cs
@@ -1,11 +1,19 @@
-﻿using WebStore.Domain.Entities.Base;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfases;
 
 namespace WebStore.Domain.Entities
 {
+    [Table("Sections")]
     public class Section : NamedEntity, IOrderedEntity
     {
         public int Order { set; get; }
         public int? ParentId { set; get; }
+
+        [ForeignKey(nameof(ParentId))]
+        public virtual Section ParentSections { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
