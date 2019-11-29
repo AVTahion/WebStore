072519b4280adb21ec1584f8aa5ca7e2a6d73ea9
AVTahion <avkontakt40@gmail.com>
2019-11-29 14:55:16 +0300

    Реализация регистрации нового пользователя

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
+using WebStore.ViewModels.Identity;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register() => View(new RegisterUserViewModel());
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel Model)
+        {
+            if (!ModelState.IsValid)
+                return View(Model);
+
+            var user = new User()
+            {
+                UserName = Model.UserName
+            };
+
+            var register_result = await _userManager.CreateAsync(user, Model.Password);
+            if (register_result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in register_result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View(Model);
+        }
+
+        //public IActionResult Login() => View();
+
+    }
+}
