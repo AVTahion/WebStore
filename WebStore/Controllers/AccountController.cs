a6f573fd4931a348d4445142f6b6a4295cade100
AVTahion <avkontakt40@gmail.com>
2019-11-30 11:56:15 +0300

    Система логирования в контроллере Account

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using WebStore.Domain.Entities.Identity;
 using WebStore.ViewModels.Identity;
 
@@ -13,11 +14,13 @@ namespace WebStore.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<AccountController> _logger;
 
-        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, ILogger<AccountController> logger)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _logger = logger;
         }
 
         public IActionResult Register() => View(new RegisterUserViewModel());
@@ -33,17 +36,20 @@ namespace WebStore.Controllers
                 UserName = Model.UserName
             };
 
+            _logger.LogInformation("регистрация нового пользователя {0}", Model.UserName);
+
             var register_result = await _userManager.CreateAsync(user, Model.Password);
             if (register_result.Succeeded)
             {
+                _logger.LogInformation("Пользователь {0} зарегистрирован", Model.UserName);
                 await _signInManager.SignInAsync(user, false);
+                _logger.LogInformation("Пользователь {0} вошел в систему", Model.UserName);
                 return RedirectToAction("Index", "Home");
             }
 
-            foreach (var error in register_result.Errors)
-            {
-                ModelState.AddModelError("", error.Description);
-            }
+            foreach (var error in register_result.Errors) ModelState.AddModelError("", error.Description);
+
+            _logger.LogWarning("Ошибка при регистрации пользователя {0}:{1}", Model.UserName, string.Join(", ", register_result.Errors.Select(e => e.Description)));
 
             return View(Model);
         }
@@ -64,19 +70,26 @@ namespace WebStore.Controllers
 
             if (login_result.Succeeded)
             {
+                _logger.LogInformation("Пользователь {0} вошел в систему", Model.UserName);
+
                 if (Url.IsLocalUrl(Model.ReturnUrl))
                     return Redirect(Model.ReturnUrl);
                 return RedirectToAction("Index", "Home");
             }
 
             ModelState.AddModelError("", "Неверное имя пользователя, или пароль");
+
+            _logger.LogWarning("Ошибка при входе пользователя {0}", Model.UserName);
+
             return View(Model);
         }
 
         [HttpPost, ValidateAntiForgeryToken]
         public async Task<IActionResult> Logout()
         {
+            var user_name = User.Identity.Name;
             await _signInManager.SignOutAsync();
+            _logger.LogInformation("Пользователь {0} вышел из системы", user_name);
             return RedirectToAction("Index", "Home");
         }
 
