62e2473503e9923c1e4a36f8c85822e5d8a83af7
AVTahion <avkontakt40@gmail.com>
2019-12-04 15:32:36 +0300

    Реализация корзины на базе cookies

---
+++ WebStore/Controllers/CartController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.infrastucture.interfaces;
+
+namespace WebStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService) => _cartService = cartService;
+
+        public IActionResult Details() => View(_cartService.TransformFromCart());
+
+        public IActionResult AddToCart(int id)
+        {
+            _cartService.AddToCart(id);
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult DecrimentFromCart(int id)
+        {
+            _cartService.DecrementFromCart(id);
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult RemoveFromCart(int id)
+        {
+            _cartService.RemoveFromCart(id);
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult RemovAll()
+        {
+            _cartService.RemoveAll();
+            return RedirectToAction("Details");
+        }
+
+    }
+}
---
+++ WebStore/Models/CartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Internal;
+using WebStore.ViewModels;
+
+namespace WebStore.Models
+{
+    public class CartViewModel
+    {
+        public Dictionary<ProductViewModel, int> Items { get; set; } = new Dictionary<ProductViewModel, int>();
+
+        public int ItemsCoumt => Items?.Sum(item => item.Value) ?? 0;
+    }
+}
