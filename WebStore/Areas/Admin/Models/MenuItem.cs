ac6cd75d424ed1bf7125cb69baf48823a3d72606
AVTahion <avkontakt40@gmail.com>
2019-12-07 14:19:36 +0300

    Удаление мусора

--- WebStore/Areas/Admin/Models/MenuItem.cs
+++ WebStore/Areas/Admin/Models/MenuItem.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebStore.Areas.Admin.Models
+﻿namespace WebStore.Areas.Admin.Models
 {
     public class MenuItem
     {
