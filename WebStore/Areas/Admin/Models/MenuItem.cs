830583f3f4c4d1aa564f36a5f6fd0b1f7259810a
AVTahion <avkontakt40@gmail.com>
2019-12-07 14:13:41 +0300

    Выделение активного пункта меню навигации админки

---
+++ WebStore/Areas/Admin/Models/MenuItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Areas.Admin.Models
+{
+    public class MenuItem
+    {
+        public string Action { get; set; }
+
+        public string Ico { get; set; }
+
+        public string Title { get; set; }
+    }
+}
