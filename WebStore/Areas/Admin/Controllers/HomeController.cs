dd50e48cb792adc3d2e098037a544477dff0586f
AVTahion <avkontakt40@gmail.com>
2019-12-08 20:10:34 +0300

    Реализация меню навигации админки на атрибутах действий контроллера

--- WebStore/Areas/Admin/Controllers/HomeController.cs
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -7,22 +7,25 @@ namespace WebStore.Areas.Admin.Controllers
     [Area("Admin"), Authorize(Roles = Role.Administrator)]
     public class HomeController : Controller
     {
+        [MenuItemElements(" Dashboard", "fa fa-dashboard fa-3x")]
         public IActionResult Index() => View();
         
+        [MenuItemElements(" UI Elements", "fa fa-desktop fa-3x")]
         public IActionResult UI() => View();
 
+        [MenuItemElements(" Tabs & Panels", "fa fa-qrcode fa-3x")]
         public IActionResult Tab_panel() => View();
 
+        [MenuItemElements(" Morris Charts", "fa fa-bar-chart-o fa-3x")]
         public IActionResult Chart() => View();
 
+        [MenuItemElements(" Table Examples", "fa fa-table fa-3x")]
         public IActionResult Table() => View();
 
+        [MenuItemElements(" Forms", "fa fa-edit fa-3x")]
         public IActionResult Form() => View();
 
-        public IActionResult Login() => View();
-
-        public IActionResult Registeration() => View();
-
+        [MenuItemElements(" Blank Page", "fa fa-square-o fa-3x")]
         public IActionResult Blank() => View();
     }
 }
---
+++ WebStore/Areas/Admin/MenuItemElementsAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Areas.Admin
+{
+    [AttributeUsage( AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class MenuItemElementsAttribute : Attribute
+    {
+        public string Title { get; }
+        public string Ico { get; }
+
+        public MenuItemElementsAttribute(string title) => Title = title;
+
+        public MenuItemElementsAttribute(string title, string ico)
+        {
+            Title = title;
+            Ico = ico;
+        }
+    }
+}
