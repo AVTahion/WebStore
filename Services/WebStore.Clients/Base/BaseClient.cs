292e6e3d58002944c0ebcc5d69476e637bffde55
AVTahion <avkontakt40@gmail.com>
2020-01-23 12:16:04 +0300

    Доработка базового клиента - реализация основных методов Http

--- Services/WebStore.Clients/Base/BaseClient.cs
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WebStore.Clients.Base
 {
@@ -26,6 +28,39 @@ namespace WebStore.Clients.Base
             headers.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
+        protected T Get<T>(string url) where T : new() => GetAsync<T>(url).Result;
+
+        protected async Task<T> GetAsync<T>(string url, CancellationToken Cancel = default) where T : new()
+        {
+            var response = await _Client.GetAsync(url, Cancel);
+            if (response.IsSuccessStatusCode)
+                return await response.Content.ReadAsAsync<T>(Cancel);
+            
+            return new T();
+        }
+
+        protected HttpResponseMessage Post<T>(string url, T item) => PostAsync(url, item).Result;
+
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T item, CancellationToken Cancel = default)
+        {
+            var response = await _Client.PostAsJsonAsync(url, item, Cancel);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected HttpResponseMessage Put<T>(string url, T item) => PutAsync(url, item).Result;
+
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item, CancellationToken Cancel = default)
+        {
+            var response = await _Client.PutAsJsonAsync(url, item, Cancel);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
+
+        protected async Task<HttpResponseMessage> DeleteAsync(string url, CancellationToken Cancel = default) =>
+            await _Client.DeleteAsync(url, Cancel);
+
+        #region Disposable
         public void Dispose()
         {
             Dispose(true);
@@ -41,5 +76,7 @@ namespace WebStore.Clients.Base
 
             _Client.Dispose();
         }
+
+        #endregion
     }
 }
