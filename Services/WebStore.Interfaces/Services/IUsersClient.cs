27b72cae6ed225a5fe56114c6cc481fb7c44ca90
AVTahion <avkontakt40@gmail.com>
2020-01-25 13:36:24 +0300

    Интерфейсы клиентов системы Identity

---
+++ Services/WebStore.Interfaces/Services/IUsersClient.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Interfaces.Services
+{
+    public interface IUsersClient :
+        IUserRoleStore<User>,
+        IUserPasswordStore<User>,
+        IUserPhoneNumberStore<User>,
+        IUserEmailStore<User>,
+        IUserClaimStore<User>,
+        IUserTwoFactorStore<User>,
+        IUserLoginStore<User>,
+        IUserLockoutStore<User>
+    {
+    }
+}
